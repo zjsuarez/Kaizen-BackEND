ef0f4e7293ba10faabd10e813ea6abc9e8cc353f
zjsuarez <zjs.suarez@gmail.com>
2026-03-06 18:28:56 +0100

    First Commit

---
+++ src/main/java/com/kaizen/gym_api/KaizenApiApplication.java
@@ -0,0 +1,13 @@
+package com.kaizen.gym_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KaizenApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KaizenApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kaizen/gym_api/KaizenApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaizen.gym_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KaizenApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
